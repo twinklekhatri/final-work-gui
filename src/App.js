97e106e0b25daa1b61b2eabccdc9bb14b201907d
twinkle <ttkhatri12@gmail.com>
2024-02-22 20:18:10 +0530

    side bar added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/Shared/NavBar';
 import Footer from './Components/Shared/Footer';
+import SideBar from './Components/Shared/SideBar';
 
 function App() {
   return (<>
    <NavBar/>
    <Footer/>
+   <SideBar/>
    </>
   );
 }
